0de832e6f1d5c115a46444775a542f2b493703cc
Satu <gayasatum@gmail.com>
2021-05-29 19:02:26 +0300

    kurssi1.5 valmis

--- viikko1/kurssitiedo/kurssitiedot/src/Content.js
+++ viikko1/kurssitiedo/kurssitiedot/src/Content.js
@@ -3,24 +3,26 @@ import Part from './Part'
 import Total from './Total'
 
 const Content = () => {
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-      }
-      const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-      }
-      const part3 = {
-        name: 'State of a component',
-        exercises: 14
-      }
+    const parts = [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
   return (
     <div>
-        <Part part={part1.name} exercise={part1.exercises}/>
-        <Part part={part2.name} exercise={part2.exercises}/>
-        <Part part={part3.name} exercise={part3.exercises}/>
-        <Total sum={part1.exercises + part2.exercises +  part3.exercises} />
+        <Part part={parts[0].name} exercise={parts[0].exercises}/>
+        <Part part={parts[1].name} exercise={parts[1].exercises}/>
+        <Part part={parts[2].name} exercise={parts[2].exercises}/>
+        <Total sum={parts[0].exercises + parts[1].exercises +  parts[2].exercises} />
      </div>
   )
 }
--- viikko1/kurssitiedo/kurssitiedot/src/Part.js
+++ viikko1/kurssitiedo/kurssitiedot/src/Part.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Part = (props) => {
-  
+    console.log(props)
+    
   return (
     <div>
       {props.part}{props.exercise} 
+      
      </div>
   )
 }
