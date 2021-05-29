31a93b35ad5ea0d9aad961e5ee346afa906d1245
Satu <gayasatum@gmail.com>
2021-05-29 16:48:18 +0300

    Kurssitiedot1.1

---
+++ viikko1/kurssitiedo/kurssitiedot/src/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Content = (props) => {
+  
+
+  return (
+    <div>
+      {props.part} {props.exercises} 
+     </div>
+  )
+}
+
+export default Content
---
+++ viikko1/kurssitiedo/kurssitiedot/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+  
+  return (
+    <div>
+      {props.course} 
+     </div>
+  )
+}
+
+export default Header
